253727e67657adba46940f8ac53a14f3ed6ec102
Aosanders <sanderscross@gmail.com>
2018-04-06 16:52:16 -0700

    Added Loade panel graphics

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 window.addEventListener("DOMContentLoaded", function(){
-    var flipPanels = document.querySelector(".hero").childNodes;
+    let flipPanels = document.querySelector(".hero").children;
+
+    panelDown(0);
+
+    function panelDown(index){
+        if(index < flipPanels.length){
+            setTimeout(function(){flipPanels[index].classList.add("move-panel");}, 200);
+            setTimeout(function(){panelDown(++index);}, 200);
+        }
+    }
 
 
 });
