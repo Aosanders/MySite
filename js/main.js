d7bd5acc97ac60fcb810cbc767b1adcda44f1f6e
Aosanders <sanderscross@gmail.com>
2018-04-09 16:28:49 -0700

    working on flip animation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 window.addEventListener("DOMContentLoaded", function(){
     let flipPanels = document.querySelector(".hero").children;
 
-    panelDown(0);
+    panelDown(0, turnPanel);
+    turnPanel((flipPanels.length-1));
 
     function panelDown(index){
         if(index < flipPanels.length){
             setTimeout(function(){flipPanels[index].classList.add("move-panel");}, 200);
             setTimeout(function(){panelDown(++index);}, 200);
+        }
+    }
+
+    function turnPanel(index){
+        if(index >= 0){
+            setTimeout(function(){flipPanels[index].classList.add("blinds-panel");}, 200);
+            setTimeout(function(){turnPanel(--index);}, 200);
         }
     }
 
