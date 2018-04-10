bb7a832bb03b4242683f1727915fd5e74e78c330
Aosanders <sanderscross@gmail.com>
2018-04-10 16:05:26 -0700

    finished panel flip animation using recursion. Messy

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 window.addEventListener("DOMContentLoaded", function(){
     let flipPanels = document.querySelector(".hero").children;
 
-    panelDown(0, turnPanel);
-    turnPanel((flipPanels.length-1));
+    panelDown(0);
+
 
     function panelDown(index){
-        if(index < flipPanels.length){
-            setTimeout(function(){flipPanels[index].classList.add("move-panel");}, 200);
-            setTimeout(function(){panelDown(++index);}, 200);
+        if(index === flipPanels.length){
+            turnPanel((flipPanels.length-1));
+            return;
         }
+        setTimeout(function(){flipPanels[index].classList.add("move-panel");}, 200);
+        setTimeout(function(){panelDown(++index);}, 200);
     }
 
     function turnPanel(index){
