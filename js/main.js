480007c75592b263d8c29c070827eb0c2f4bdf70
Aosanders <sanderscross@gmail.com>
2018-04-13 15:50:55 -0700

    working on element remove-hero using Promises

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 window.addEventListener("DOMContentLoaded", function(){
-    let flipPanels = document.querySelector(".hero").children;
+    let flipPanels = document.querySelector(".hero");
 
     panelDown(0);
 
 
     function panelDown(index){
-        if(index === flipPanels.length){
-            turnPanel((flipPanels.length-1));
-            return;
+        if(index === flipPanels.children.length){
+            return setTimeout(()=>turnPanel((flipPanels.children.length-1)), 500);
         }
-        setTimeout(function(){flipPanels[index].classList.add("move-panel");}, 200);
-        setTimeout(function(){panelDown(++index);}, 200);
+        setTimeout(()=>flipPanels.children[index].classList.add("move-panel"), 200);
+        setTimeout(()=>panelDown(++index), 200);
     }
 
     function turnPanel(index){
         if(index >= 0){
-            setTimeout(function(){flipPanels[index].classList.add("blinds-panel");}, 200);
-            setTimeout(function(){turnPanel(--index);}, 200);
+            setTimeout(()=>flipPanels.children[index].classList.add("blinds-panel"), 200);
+            setTimeout(()=>turnPanel(--index), 200);
         }
     }
 
