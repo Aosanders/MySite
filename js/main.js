c134f6050bc2f4a6b1cd9a55eebbb8ceb5d1ea27
Aosanders <sanderscross@gmail.com>
2018-04-16 15:47:06 -0700

    Added func that remove panel elements

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ window.addEventListener("DOMContentLoaded", function(){
 
     function panelDown(index){
         if(index === flipPanels.children.length){
-            return setTimeout(()=>turnPanel((flipPanels.children.length-1)), 500);
+            setTimeout(()=>turnPanel((flipPanels.children.length-1)), 500);
+            return;
         }
         setTimeout(()=>flipPanels.children[index].classList.add("move-panel"), 200);
         setTimeout(()=>panelDown(++index), 200);
@@ -17,6 +18,19 @@ window.addEventListener("DOMContentLoaded", function(){
             setTimeout(()=>flipPanels.children[index].classList.add("blinds-panel"), 200);
             setTimeout(()=>turnPanel(--index), 200);
         }
+
+        else{
+            removePanels();
+        }
+    }
+
+    function removePanels(){
+       let panels = flipPanels.getElementsByClassName("flip-panel");
+       setTimeout(()=>{
+            while(panels.length != 0){
+                flipPanels.removeChild(panels[0]);
+            }
+        },400);
     }
 
 
